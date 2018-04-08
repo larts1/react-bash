842c760cd6bcabdefdc402c067ea07e7c3c6af86
Lauri Mäntysaari <lauri@mantysaari.net>
2018-04-08 19:04:24 +0300

    Update styles.js
    Auto max width

--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,9 @@
-const BaseStyles = {};
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var BaseStyles = {};
 
 BaseStyles.ReactBash = {
     borderRadius: '5px',
@@ -8,42 +13,45 @@ BaseStyles.ReactBash = {
     fontSize: '13px',
     fontWeight: '400',
     height: '100%',
+	width: '100%',
     overflow: 'hidden',
-    textAlign: 'left',
+    textAlign: 'left'
 };
 
 BaseStyles.header = {
-    padding: '5px 10px 0',
+    padding: '5px 10px 0'
 };
 
-const circle = {
+var circle = {
     borderRadius: '50%',
     display: 'inline-block',
     height: '15px',
     marginRight: '5px',
-    width: '15px',
+    width: '15px'
 };
 
 BaseStyles.redCircle = Object.assign({}, circle, {
-    backgroundColor: '#bf616a',
+    backgroundColor: '#bf616a'
 });
 
 BaseStyles.yellowCircle = Object.assign({}, circle, {
-    backgroundColor: '#ebcb8b',
+    backgroundColor: '#ebcb8b'
 });
 
 BaseStyles.greenCircle = Object.assign({}, circle, {
-    backgroundColor: '#a3be8c',
+    backgroundColor: '#a3be8c'
 });
 
 BaseStyles.body = {
     flexGrow: 1,
     overflowY: 'scroll',
     padding: '10px',
+	height: '100%'
 };
 
 BaseStyles.form = {
-    display: 'flex',
+    display: 'flex'
+	
 };
 
 BaseStyles.input = {
@@ -54,36 +62,36 @@ BaseStyles.input = {
     fontFamily: 'inherit',
     fontSize: 'inherit',
     outline: 'none !important',
-    padding: 0,
+    padding: 0
 };
 
 BaseStyles.prefix = {
-    marginRight: '5px',
+    marginRight: '5px'
 };
 
-export default {
+exports.default = {
     light: Object.assign({}, BaseStyles, {
         body: Object.assign({}, BaseStyles.body, {
             backgroundColor: '#fff',
-            color: '#5D5D5D',
+            color: '#5D5D5D'
         }),
         header: Object.assign({}, BaseStyles.header, {
-            backgroundColor: '#eee',
+            backgroundColor: '#eee'
         }),
         prefix: Object.assign({}, BaseStyles.prefix, {
-            color: '#bd081c',
-        }),
+            color: '#bd081c'
+        })
     }),
     dark: Object.assign({}, BaseStyles, {
         body: Object.assign({}, BaseStyles.body, {
             backgroundColor: '#000',
-            color: '#d0d0d0',
+            color: '#d0d0d0'
         }),
         header: Object.assign({}, BaseStyles.header, {
-            backgroundColor: '#dcdbdb',
+            backgroundColor: '#dcdbdb'
         }),
         prefix: Object.assign({}, BaseStyles.prefix, {
-            color: '#5b65fb',
-        }),
-    }),
+            color: '#5b65fb'
+        })
+    })
 };
